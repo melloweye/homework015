1609f83c69c0df4bc73688f79e516147b0523a61
andrew <sten1663@gmail.com>
2024-08-25 19:59:44 +0300

    homework015 - done

---
+++ src/main/java/ru/innopolis/java/App.java
@@ -0,0 +1,13 @@
+package ru.innopolis.java;
+
+/**
+ * Hello World from Jar Archive!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Jar Archive!" );
+    }
+}
